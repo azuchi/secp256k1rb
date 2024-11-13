2a9b53b014e09caa267901879e696dbd80500b28
azuchi <azuchi@chaintope.com>
2024-11-13 14:09:45 +0900

    Remove rec argument in recover

--- lib/secp256k1/recovery.rb
+++ lib/secp256k1/recovery.rb
@@ -33,30 +33,31 @@ module Secp256k1
 
     # Recover public key from compact signature.
     # @param [String] data The 32-byte message hash assumed to be signed.
-    # @param [String] signature The signature with binary format.
-    # @param [Integer] rec recovery id.
+    # @param [String] signature The signature with binary format (65 bytes).
     # @param [Boolean] compressed whether compressed public key or not.
     # @return [String] Recovered public key with hex format.
     # @raise [Secp256k1::Error] If recover failed.
     # @raise [ArgumentError] If invalid arguments specified.
-    def recover(data, signature, rec, compressed)
+    def recover(data, signature, compressed)
       raise ArgumentError, "data must be String." unless data.is_a?(String)
-      raise ArgumentError, "rec must be Integer." unless rec.is_a?(Integer)
-      raise ArgumentError, "rec must be between 0 and 3." if rec < 0 || rec > 3
       raise ArgumentError, "signature must be String." unless signature.is_a?(String)
       signature = hex2bin(signature)
-      raise ArgumentError, "signature must be 64 bytes." unless signature.bytesize == 64
+      raise ArgumentError, "signature must be 64 bytes." unless signature.bytesize == 65
       data = hex2bin(data)
       raise ArgumentError, "data must be 32 bytes." unless data.bytesize == 32
+      rec = signature[0].ord - 0x1b
+      rec -= 4 if compressed
+      raise ArgumentError, "rec must be between 0 and 3." if rec < 0 || rec > 3
 
       with_context do |context|
         sig = FFI::MemoryPointer.new(:uchar, 65)
-        input = FFI::MemoryPointer.new(:uchar, 64).put_bytes(0, signature)
+        input = FFI::MemoryPointer.new(:uchar, 64).put_bytes(0, signature[1..-1])
         result = secp256k1_ecdsa_recoverable_signature_parse_compact(context, sig, input, rec)
         raise Error, 'secp256k1_ecdsa_recoverable_signature_parse_compact failed.' unless result == 1
 
         pubkey = FFI::MemoryPointer.new(:uchar, 64)
         msg = FFI::MemoryPointer.new(:uchar, data.bytesize).put_bytes(0, data)
+        sig = FFI::MemoryPointer.new(:uchar, 65).put_bytes(0, signature)
         result = secp256k1_ecdsa_recover(context, pubkey, sig, msg)
         raise Error, 'secp256k1_ecdsa_recover failed.' unless result == 1
 
